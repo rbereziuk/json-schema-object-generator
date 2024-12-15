a025767e5bdf4eed725be8f5b1fb1398104f9155
Roman Bereziuk <roma.physicist@gmail.com>
2024-12-15 17:00:14 +0200

    Generate simple object with number and string

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Generate object');
+const schema = {
+  type: 'objectl',
+  properties: {
+    productId: {
+      description: 'The unique identifier for a product',
+      type: 'integer',
+    },
+    productName: {
+      description: 'Name of the product',
+      type: 'string',
+    },
+  },
+};
+
+function generateRandomObject(schema) {
+  const resultObj = {};
+
+  for (const property in schema.properties) {
+    resultObj[property] = generateRandomData(schema.properties[property]);
+  }
+
+  return resultObj;
+}
+
+function generateRandomData(propertySchema) {
+  switch (propertySchema.type) {
+    case 'integer':
+      return Math.floor(Math.random() * 100);
+    case 'string':
+      return 'random string';
+  }
+}
+
+console.log(generateRandomObject(schema));
